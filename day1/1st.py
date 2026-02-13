6b4321c903babaee743b2787b7d493d079969a1c
Adith sajeev <Adith sajeev@example.com>
2026-02-13 20:22:31 +0530

    Remove unnecessary files, keep only booboo2.py

--- day1/1st.py
+++
@@ -1,2 +0,0 @@
-print("This time it is acutally going to work no going back for this time." \
-"I got this.")
--- py project/makerectangle.py
+++
@@ -1,9 +0,0 @@
-rows = int(input("Enter the number of rows: "))
-coloums = int(input("Enter the number of coloums: "))
-symbol = input("Enter a symbol to use: ")
-
-for i in range(rows):
-    for x in range(coloums):
-        print(symbol, end="")
-    
-    print()
